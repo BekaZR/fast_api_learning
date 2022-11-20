9de7192efd9021377e55cc505bc36bd0f50f7fb8
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-11-20 12:20:47 +0600

    working with query parametrs

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 from fastapi import FastAPI
 
-from enum import Enum
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello world"}
+fake_product_db = [
+    {
+        "name": "Apple",
+        "price": 12.2
+        },
+    {
+        "name": "Box",
+        "price": 11.2
+        },
+    {
+        "name": "Phone",
+        "price": 12222.2
+        },
+]
 
-@app.get("/item/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/product/")
+def get_product(skip: int = 0, limit: int = 2):
+    return {"product": fake_product_db[skip : skip + limit]}
