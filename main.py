e96027218e1960f621d29e94d834dc6e0c3dff8e
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-11-20 12:13:21 +0600

    working with enum

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 app = FastAPI()
+
 
 @app.get("/")
 def root():
@@ -9,3 +19,14 @@ def root():
 @app.get("/item/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
