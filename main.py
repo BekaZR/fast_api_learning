c3140a3fec37cba5c11fb4a19cee6c721df3a4de
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-11-20 12:30:08 +0600

    use pydantic validation

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
+from unicodedata import numeric
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+class Product(BaseModel):
+    name: str
+    price: int
+    
 app = FastAPI()
 
-
-fake_product_db = [
-    {
-        "name": "Apple",
-        "price": 12.2
-        },
-    {
-        "name": "Box",
-        "price": 11.2
-        },
-    {
-        "name": "Phone",
-        "price": 12222.2
-        },
-]
-
-@app.get("/product/")
-def get_product(skip: int = 0, limit: int = 2):
-    return {"product": fake_product_db[skip : skip + limit]}
+@app.post("/product/")
+def create_product(product: Product):
+    return product
