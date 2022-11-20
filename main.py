e6bf8ee5eb1dc764b6fd4ff19657dd88540acbbf
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-11-20 20:26:26 +0600

    working with nesting pydantic

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
@@ -7,11 +14,17 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
 
 
-app = FastAPI()
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
 
 
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
